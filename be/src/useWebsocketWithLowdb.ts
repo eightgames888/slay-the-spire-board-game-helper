3c388f675c5a369d175abb2b948648359dd1a8e6
Kaiyan-Zhang <zhangkaiyan3@gmail.com>
2026-01-30 22:05:24 +0800

    what ever

--- be/src/useWebsocketWithLowdb.ts
+++ be/src/useWebsocketWithLowdb.ts
@@ -3,34 +3,42 @@ import { WebSocketServer } from "ws";
 import { JSONFilePreset } from "lowdb/node";
 
 export async function useWebsocketWithLowdb<T>(app: any, defaultData: T) {
-	const db = await JSONFilePreset<{
-		data: T;
-	}>("db.json", { data: defaultData });
+  const db = await JSONFilePreset<{
+    data: T;
+  }>("db.json", { data: defaultData });
 
-	const server = createServer(app);
-	const wss = new WebSocketServer({ server });
+  const server = createServer(app);
+  const wss = new WebSocketServer({ server });
 
-	wss.on("connection", (ws) => {
-		console.log("Websocket connected");
-		
-		ws.send(JSON.stringify({ type: "sync", value: db.data.data }));
+  wss.on("connection", (ws) => {
+    console.log("Websocket connected");
 
-		ws.on("message", async (message) => {
-			const { type, value } = JSON.parse(message.toString());
-			if (type === "update") {
-				await db.update((draft) => {
-					draft.data = value;
-				});
-				wss.clients.forEach((client) => {
-					if (client.readyState === 1) {
-						client.send(JSON.stringify({ type: "sync", value: db.data.data }));
-					}
-				})
-			}
-		});
-	});
-	wss.on("close", () => {
-		console.log("Websocket closed");
-	});
-	return { db, server, wss };
+    ws.send(JSON.stringify({ type: "sync", value: db.data.data }));
+
+    ws.on("close", (code) => {
+      console.log(`Websocket disconnected: ${code}`);
+    });
+
+    ws.on("error", (error) => {
+      console.error("Websocket error:", error);
+    });
+
+    ws.on("message", async (message) => {
+      const { type, value } = JSON.parse(message.toString());
+      if (type === "update") {
+        await db.update((draft) => {
+          draft.data = value;
+        });
+        wss.clients.forEach((client) => {
+          if (client.readyState === 1) {
+            client.send(JSON.stringify({ type: "sync", value: db.data.data }));
+          }
+        });
+      }
+    });
+  });
+  wss.on("close", () => {
+    console.log("Websocket closed");
+  });
+  return { db, server, wss };
 }
